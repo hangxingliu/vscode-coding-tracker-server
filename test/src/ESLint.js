599bb8befb748d76d653244afd5015be608d5971
LiuYue <hangxingliu@gmail.com>
2017-07-20 07:50:43 +0800

    prepare for new unit test(eslint, tsc)

---
+++ test/src/ESLint.js
@@ -0,0 +1,13 @@
+// Code Review by ESLint
+// No warning and error allow!
+
+const ESLINT_RC = `${__dirname}/../../.eslintrc.json`;
+
+let fs = require('fs');
+
+let ESLint = require('eslint');
+
+ESLint.Linter;
+ESLint.CLIEngine;
+
+// TODO
---
+++ test/src/TypescriptCheck.js
@@ -0,0 +1,7 @@
+// Code Review by tsc (Typescript Check)
+// No warning and error allow!
+
+let fs = require('fs'),
+	{ exec } = require('child_process');
+
+// TODO
