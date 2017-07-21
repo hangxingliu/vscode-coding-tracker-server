4a5d548ec6ec60763b0d3acdde7aac64a0f2a98d
LiuYue <hangxingliu@gmail.com>
2017-07-22 02:13:20 +0800

    feed some words to cSpell and fix a spell mistake

--- frontend/src/js/echartsUtils.js
+++ frontend/src/js/echartsUtils.js
@@ -3,7 +3,7 @@ module.exports = {
 	AXIS_HOURS: { axisLabel: { formatter: '{value} hs' } },
 	AXIS_MINUTES: { axisLabel: { formatter: '{value} mins' } },
 	GRID_NORMAL: { left: '20', right: '20', bottom: '10', top: '10', containLabel: true },
-	GRID_HORIZON_BAR: {left: '10', right: '20', bttom: '5', top: '5', containLabel: true}
+	GRID_HORIZON_BAR: {left: '10', right: '20', bottom: '5', top: '5', containLabel: true}
 };
 
 /**
