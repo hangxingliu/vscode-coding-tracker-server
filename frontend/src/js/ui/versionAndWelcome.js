a5396af1540b4b063c26609bb85ee7f233cc7edc
LiuYue <hangxingliu@gmail.com>
2017-11-24 17:15:14 +0800

    Sidebar item.Config sourcemap.Export in versionAndWelcome.

--- frontend/src/js/ui/versionAndWelcome.js
+++ frontend/src/js/ui/versionAndWelcome.js
@@ -8,6 +8,8 @@ $('.btn-show-welcome').on('click', showWelcome);
 
 init();
 
+module.exports = { show: showWelcome };
+
 function hideWelcome() { $welcomeInfo.slideUp(); localStorage.setItem(VERSION_KEY, currentServerVersion); }
 function showWelcome() { $welcomeInfo.slideDown(); }
 function init() {
